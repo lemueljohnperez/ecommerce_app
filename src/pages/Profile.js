8df6e821fc11552d5ad6f75265b4bc3e3b841d3e
lemueljohnperez0 <lemueljohnperez0@gmail.com>
2024-03-26 22:23:54 +0800

    Fixed profile function

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -14,7 +14,7 @@ export default function Profile(){
 
     useEffect(()=>{
 
-        fetch(`http://localhost:4000/users/`,{
+        fetch(`${process.env.REACT_APP_API_URL}/users/details`,{
             headers:{
                 Authorization: `Bearer ${ localStorage.getItem('token')}`
             }
